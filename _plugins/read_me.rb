36fb2eb96b27ce287f5192a999a63525f1fa9889
Jorge García <jorge.em223@gmail.com>
2012-06-07 14:40:51 -0500

    agregado LEER MAS

---
+++ _plugins/read_me.rb
@@ -0,0 +1,25 @@
+module Jekyll
+  
+  class Post
+    alias_method :original_to_liquid, :to_liquid
+    def to_liquid
+      original_to_liquid.deep_merge({
+        'excerpt' => content.match('<!--more-->') ? content.split('<!--more-->').first : nil
+      })
+    end
+  end
+  
+  module ReadMe
+    
+    def truncate content
+      more_tag = '<!--more-->'
+      if content.match more_tag
+        content = content.match(more_tag) ? content.split('<!--more-->').first : nil
+        content += '... '
+      end
+      content
+    end
+  end
+end
+
+Liquid::Template.register_filter(Jekyll::ReadMe)
