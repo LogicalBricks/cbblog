284a2447f9ec3707b900d2d17fcb907f1c5d8ecf
Jorge García <jorge.em223@gmail.com>
2012-11-03 18:32:20 -0700

    Agregando archivos con formato HAML - Los posts funcionan perfectamente - Los layouts no encuentran los objectos, parece ser que es un problema que   se tiene con HAML. - No funciona usando HAML en layouts.
    TODO: Buscar la solución más apropiada para la implementación de
    	  HAML en los Layouts.

---
+++ _plugins/haml_converter.rb
@@ -0,0 +1,48 @@
+module Jekyll
+  require 'haml'
+  class HamlConverter < Converter
+    safe true
+    priority :low
+
+    def matches(ext)
+      ext =~ /haml/i
+    end
+
+    def output_ext(ext)
+      ".html"
+    end
+
+    def convert(content)
+      begin
+        engine = Haml::Engine.new(content)
+        engine.render
+      rescue StandardError => e
+        puts "!!! HAML Error: #{e}"
+      end
+    end
+  end
+
+  require 'sass'
+  class SassConverter < Converter
+    safe true
+    priority :low
+
+    def matches(ext)
+      ext =~ /sass/i
+    end
+
+    def output_ext(ext)
+      ".css"
+    end
+
+    def convert(content)
+      begin
+        puts "Performing Sass Conversion."
+        engine = Sass::Engine.new(content, :syntax => :sass, :load_paths => ["./css/"])
+        engine.render
+      rescue StandardError => e
+        puts "!!! SASS Error: " + e.message
+      end
+    end
+  end
+end
--- _posts/2012-06-06-escribiendo-desde-consola.markdown
+++
@@ -1,5 +0,0 @@
----
-layout: post
-title: Escribiendo desde consola
----
-This is a small example writting a document with **markdowns**
